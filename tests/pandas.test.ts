a2664f09bc859619fd1a74e0b118d6a80fabd8cc
Giacomo Tagliabue <giacomo.tag@gmail.com>
2020-08-05 22:33:14 -0400

    fix tests tz (#6)

--- tests/pandas.test.ts
+++ tests/pandas.test.ts
@@ -1,9 +1,14 @@
+import { Settings } from 'luxon';
+
 import { createPythonEnv } from '../src';
 import { addPandasDecoders } from '../src/pandas';
 import { PythonEnv, kwargs } from '../src/py';
 import { PYTHON_PATH } from './config';
 
 let py: PythonEnv;
+
+// to make the snapshots deterministic in any timezone
+Settings.defaultZoneName = 'UTC';
 
 beforeEach(async () => {
   py = createPythonEnv({ pythonPath: PYTHON_PATH });
@@ -31,8 +36,8 @@ test('series', async () => {
   ]);
   expect(await dateSeries._).toMatchInlineSnapshot(`
     Array [
-      "2020-01-01T00:00:00.000-05:00",
-      "2020-01-02T00:00:00.000-05:00",
+      "2020-01-01T00:00:00.000Z",
+      "2020-01-02T00:00:00.000Z",
     ]
   `);
 });
@@ -57,45 +62,45 @@ test('dataframe', async () => {
       Object {
         "a": 0,
         "b": "a",
-        "c": "2019-01-01T00:00:00.000-05:00",
+        "c": "2019-01-01T00:00:00.000Z",
         "d": "PT-432000S",
         "e": Object {},
       },
       Object {
         "a": 1,
         "b": "b",
-        "c": "2019-01-02T00:00:00.000-05:00",
+        "c": "2019-01-02T00:00:00.000Z",
         "d": "PT-259200S",
         "e": Array [],
       },
       Object {
         "a": 2,
         "b": "c",
-        "c": "2019-01-03T00:00:00.000-05:00",
+        "c": "2019-01-03T00:00:00.000Z",
         "d": "PT-86400S",
         "e": Array [],
       },
       Object {
         "a": 3,
         "b": "d",
-        "c": "2019-01-04T00:00:00.000-05:00",
+        "c": "2019-01-04T00:00:00.000Z",
         "d": "PT86400S",
         "e": 4,
       },
       Object {
         "a": 4,
         "b": "e",
-        "c": "2019-01-05T00:00:00.000-05:00",
+        "c": "2019-01-05T00:00:00.000Z",
         "d": "PT259200S",
         "e": "-",
       },
       Object {
         "a": 5,
         "b": "f",
-        "c": "2019-01-06T00:00:00.000-05:00",
+        "c": "2019-01-06T00:00:00.000Z",
         "d": "PT432000S",
         "e": Array [
-          "2020-01-01T00:00:00.000-05:00",
+          "2020-01-01T00:00:00.000Z",
         ],
       },
     ]
